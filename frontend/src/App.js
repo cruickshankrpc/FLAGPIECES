3a05497ed6417c5e8f9455d21757bedd78179307
Raquel <cruickshankrpc@gmail.com>
2020-07-24 12:05:55 +0100

    frontend folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import axios from axios 
 
 const App = () => {
   return <h1>Hello World</h1>
-}
 
+  const articles = (() => {
+    axios.get(`https://newsapi.org/v2/everything?q=  ${country} & ${  API_KEY  }`)
+
+  })
+
+}
+  
 export default App
