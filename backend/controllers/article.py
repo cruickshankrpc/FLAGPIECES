6c5b89fb2a5c2221cade8f55a868706ee3973fa2
RichardBekoe <richardbekoe@gmail.com>
2020-07-24 16:15:47 +0100

    controllers single article

--- backend/controllers/article.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint, request, jsonify, g
-from models.user import User, UserSchema 
-from app import db
---
+++ backend/controllers/single_article.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, jsonify, g
+from models.article import Article, ArticleSchema, Flag, FlagSchema, Comment, CommentSchema
+from models.reaction import Reaction, ReactionSchema
+from app import db
+from lib.secure_route import secure_route
+from marshmallow import ValidationError
+
+article_schema = ArticleSchema()
+comment_schema = CommentSchema()
+flag_schema = FlagSchema()
+
+router = Blueprint(__name__, 'articles')
+
+@router.route('/singlearticle/<int:id>', methods=['GET'])
+def show(id):
+  article = Article.query.get(id)
+      
+  if not article:
+    return jsonify({'message': 'article not shown'}), 404
+
+  return article_schema.jsonify(article), 200
+
+
+@router.route('/teas', methods=['POST'])
+@secure_route
+def create():
+    json_im_posting = request.get_json()
+    json_im_posting['drinker_id'] = g.current_user.id
+
+    try:
+        tea = tea_schema.load(json_im_posting)
+    except ValidationError as e:
+        return jsonify({'errors': e.messages, 'message': 'Something went wrong!'})
+
+    tea.save()
+    return tea_schema.jsonify(tea), 201
+
+
+
+
+
+
+
+
+
+
+# !to adapt code for articles
+
+# ? Why use article_id here but not with when getting the article i.e. we only use <int:id>
+# ? Could be a convention of Blueprint/ router
+
+@router.route('/singlearticle/<int:article_id>/comments', methods=['POST'])
+def comment_create(article_id):
+    comment_data = request.get_json()
+    article = article.query.get(article_id)
+    comment = comment_schema.load(comment_data)
+    # At this stage, comment has only comment.content !!!
+    # ! This tells sqlalchemy which article our comment is associated with
+    comment.article = article
+    # At this stage, the comment is complete
+    comment.save()
+    return comment_schema.jsonify(comment)
